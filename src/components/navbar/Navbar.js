f97ee7e73e6f6d4f27ea38c214a3370a7a3be276
kawsikan <kawsinote@gmail.com>
2021-09-07 19:38:41 +0530

    inital commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
                  <Link to="/table">Table</Link>  
                  <Link to="/about">About</Link>  
                  <Link to="/menu">Menu</Link>  
-                 <Link to="/review">Review</Link>  
+                 <Link to="/offer">Review</Link>  
                  <Link to="/order">Order</Link>  
              </div>
 
---
+++ src/pages/promotion/Promotion.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Promotion(){
+    return (
+        <div>
+            <h1>This Promotions</h1>
+            <h1>This Promotions</h1>
+
+            <h1>This Promotions</h1>
+
+            <h1>This Promotions</h1>
+
+        </div>
+    )
+}
