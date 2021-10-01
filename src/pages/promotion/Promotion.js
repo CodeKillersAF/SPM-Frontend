078badd1e67c7e891ca71e0df11c0e02d993fd6f
tharushadilmith <tharushadilmith99.com>
2021-10-01 21:57:40 +0530

    Resolved conflicts

--- src/pages/promotion/Promotion.js
+++ src/pages/promotion/Promotion.js
@@ -1,38 +1,24 @@
 import React from "react";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Promotion() {
+  const [showAllOffers, setShowAllOffers] = useState([]);
 
-    const [showAllOffers, setShowAllOffers] = useState([]);
+  const getAllOffers = () => {
+    axios.get("http://localhost:8000/api/offer").then((response) => {
+      console.log(response.data.data);
+      setShowAllOffers(response.data.data);
+    });
+  };
 
-    const getAllOffers = () => {
-        axios.get("http://localhost:8000/api/offer")
-            .then((response) => {
-                console.log(response.data.data);
-                setShowAllOffers(response.data.data);
-            })
-    }
+  useEffect(() => {
+    getAllOffers();
+  }, []);
 
-    useEffect(() => {
-        getAllOffers();
-    }, []);
-
-
-
-    return (
-        <div>
-
-        <h1>H</h1>
-        <h1>H</h1>
-        <h1>H</h1>
-
-        <h1>H</h1>
-        <h1>H</h1>
-        <h1>H</h1>
-
-
-            {/* <div className="foodItemCenter">
+  return (
+    <div>
+      {/* <div className="foodItemCenter">
                 {getAllCategories.map((ac) => (
                     <div className="foodItemDiv">
                         <button className="foodItemCategory" onClick={() => getCategoryFoods(ac._id)}>
@@ -41,19 +27,18 @@ export default function Promotion() {
                     </div>
                 ))}
             </div> */}
-            {showAllOffers && showAllOffers.map(showAllOffer => {
-                return (
-                    <div key={showAllOffer.id} style={{ alignItems: 'center', margin: '20px 60px' }}>
-                        <h4>{showAllOffer.offerName}</h4>
-                        <p>{showAllOffer.offerDescription}</p>
-                    </div>
-                )
-            })}
-
-
-        </div>
-    )
-
-
-
+      {showAllOffers &&
+        showAllOffers.map((showAllOffer) => {
+          return (
+            <div
+              key={showAllOffer.id}
+              style={{ alignItems: "center", margin: "20px 60px" }}
+            >
+              <h4>{showAllOffer.offerName}</h4>
+              <p>{showAllOffer.offerDescription}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
