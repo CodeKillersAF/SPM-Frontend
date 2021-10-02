4e7684f91f07c73a8f69e22e1adb0f883ea19691
Didula-Lakminda <didulalakminda450@gmail.com>
2021-10-02 20:08:01 +0530

    loading screen designed

--- src/components/homeBody/HomeBody.js
+++ src/components/homeBody/HomeBody.js
@@ -1,43 +1,47 @@
-import React, { useEffect, useRef } from 'react';
-import './homeBody.css';
-import { init } from 'ityped';
+import React, { useEffect, useRef } from "react";
+import "./homeBody.css";
+import { init } from "ityped";
 
 function HomeBody() {
+  const textRef = useRef();
 
-    const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["Welcome To... ~!"],
+      typeSpeed: 120,
+      backSpeed: 120,
+      // loop: false,
+      // disableBackTyping: true
+    });
+  }, []);
 
-    useEffect(() => {
-        init(textRef.current, { 
-            showCursor: false, 
-            strings: ["Welcome...~! To Restaurant"],  
-        })
-    }, []);
+  return (
+    <section className="home" id="home">
+      <div className="home-slider">
+        <div className="wrapper">
+          <div className="slide">
+            <div className="content">
+              <span ref={textRef}></span>
+              <h3>Kasuki Restaurant</h3>
+              <p>Best Restaurant in the town</p>
+              <a href="#" className="btn">
+                order now
+              </a>
+            </div>
+            <div className="image">
+              <img
+                src="https://www.pngall.com/wp-content/uploads/5/Home-Interior-Design-PNG-Image.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
 
-    return (
-            <section className="home" id="home">
-                <div className="home-slider">
-                    <div className="wrapper">
-                        <div className="slide">
-                            <div className="content">
-                                <span ref={textRef}></span>
-                                <h3>SPM Project</h3>
-                                <p>Frontend design for spm project side</p>
-                                <a href="#" className="btn">order now</a>
-                            </div>
-                            <div className="image">
-                                <img
-                                src="https://www.pngall.com/wp-content/uploads/5/Home-Interior-Design-PNG-Image.png" alt="" />
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="swiper-pagination"></div>
-
-                </div>
+        <div className="swiper-pagination"></div>
+      </div>
     </section>
-
-    )
+  );
 }
 
-export default HomeBody
+export default HomeBody;
