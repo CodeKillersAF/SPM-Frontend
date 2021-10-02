732598008ba65857506c414e7529c80c8df00f63
Didula-Lakminda <didulalakminda450@gmail.com>
2021-10-02 21:41:40 +0530

    food menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import Navbar from "./components/navbar/Navbar";
+// import Footer from "./components/footer/Footer";
+ 
+
+// import Home from "./pages/home/Home";
+// import Table from "./pages/table/Table";
+// import ContactUs from "./components/contactUs/ContactUs";
+import LoadingScreen from '.././src/components/loadingScreen/LoadingScreen';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+
+import FoodMenu from "./components/foodMenu/FoodMenu";
+
+// import TestCart from "./components/testCart/TestCart";
+
+// import Table from "./components/tableTharusha/Table";
  
 
 import Home from "./pages/home/Home";
 import Table from "./pages/table/Table";
-import ContactUs from "./components/contactUs/ContactUs";
-import LoadingScreen from '.././src/components/loadingScreen/LoadingScreen';
 
 function App() {
 
@@ -27,10 +41,9 @@ function App() {
           <Navbar />
           <section>
             <Switch>
-                <Route path="/" component={Home} exact />
+            <Route path="/" component={Home} exact />
+                <Route path="/menu" component={FoodMenu} />
                 <Route path="/table" component={Table} />
-
-                <Route path="/contact" component={ContactUs} />
 
             </Switch>
           </section>
