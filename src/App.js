0941adcef21b84e64b651a72dc6e26a3eef32e45
Didula-Lakminda <didulalakminda450@gmail.com>
2021-10-02 21:04:38 +0530

    food_menu deleted some files

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-
-import FoodMenu from "./components/foodMenu/FoodMenu";
-
-import TestCart from "./components/testCart/TestCart";
-
-// import Table from "./components/tableTharusha/Table";
  
 
 import Home from "./pages/home/Home";
 import Table from "./pages/table/Table";
-import Promotion from "./pages/promotion/Promotion";
 import ContactUs from "./components/contactUs/ContactUs";
 import LoadingScreen from '.././src/components/loadingScreen/LoadingScreen';
 
@@ -35,13 +28,7 @@ function App() {
           <section>
             <Switch>
                 <Route path="/" component={Home} exact />
-                <Route path="/menu" component={FoodMenu} />
-
-                <Route path="/test" component={TestCart} />
-
-                {/* <Route path="/table" component={Table} /> */}
                 <Route path="/table" component={Table} />
-                <Route path="/offer" component={Promotion} />
 
                 <Route path="/contact" component={ContactUs} />
 
